27be40d9da5e365b2b975f49d4b533d15f398c2b
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-06-20 11:53:21 +0200

    Update MethTest.java

--- MethTest.java
+++ MethTest.java
@@ -1,18 +1,18 @@
-import java.util.ArrayList;
-
 public class MethTest extends AATest implements  TranslInterface {
 
 	  
 		    private String peptide = ">";
-		 
-		 	protected TranslInterface nextInChain;
-			protected String singleLetterCode;
-		   // protected ArrayList<String> codons; 
-		   // protected ProtTest aminoAcid = new ProtTest(codons, ""); 		
-			protected String source;
+		   
+		    // all marked instances/variables inherited from AATest superclass !
+		    
+		    // protected TranslInterface nextInChain;
+		    // protected String singleLetterCode;
+		    protected ProtTest aminoAcid = new ProtTest(codons, ""); 		
+		    // protected ArrayList<String> codons; 
+		    protected String source;
 			private SubTest substrings = new SubTest(source);
-			protected int i=0;
-			protected int j;
+			// protected int i;
+			// protected int j;
 
 			
 		// Konstruktor
@@ -20,18 +20,24 @@ public class MethTest extends AATest implements  TranslInterface {
 			this.singleLetterCode = singleLetterCode;
 		}
 
+		@Override
 		public void setNextChain(TranslInterface nextChain) {
 			this.nextInChain = nextChain;	
 		}
-		                                                                                      // toString()-Methode anpassen!! s.u.
+		                                                                                      
 		
+		@Override
 		public void compareSub(ProtTest aminoAcid) {                 
-			
-		  while( i < codons.size()) {  
+			codons = substrings.getCodonsSub();
+
+			while( i < codons.size()) {  
 			  if (codons.get(i).equals("AUG")) {                                
 				     singleLetterCode = "M";                       
 				     i+=1;	
 				     System.out.print(singleLetterCode + " Meth");
+				     peptide = peptide + singleLetterCode;
+				     aminoAcid.setPeptide(peptide);
+				     aminoAcid.setCodonsRNA(codons);
 				     }		
 	    
 	 		  else {                                        
@@ -44,7 +50,7 @@ public class MethTest extends AATest implements  TranslInterface {
 		
 		
 		@Override
-	    public String toString() {                        // das hier noch überprüfen und bei allen AS Klassen anpassen!!
+	    public String toString() {                        // das hier noch Ã¼berprÃ¼fen und bei allen AS Klassen anpassen!!
 			peptide = peptide + singleLetterCode;
 			return peptide;
 	    }
@@ -61,5 +67,39 @@ public class MethTest extends AATest implements  TranslInterface {
 		
 		}
 
+		public String getPeptide() {
+			return peptide;
+		}
+
+		public void setPeptide(String peptide) {
+			this.peptide = peptide;
+		}
+
+		public ProtTest getAminoAcid() {
+			return aminoAcid;
+		}
+
+		public void setAminoAcid(ProtTest aminoAcid) {
+			this.aminoAcid = aminoAcid;
+		}
+
+		public String getSource() {
+			return source;
+		}
+
+		public void setSource(String source) {
+			this.source = source;
+		}
+
+		public SubTest getSubstrings() {
+			return substrings;
+		}
+
+		public void setSubstrings(SubTest substrings) {
+			this.substrings = substrings;
+		}
+
 	
+	    
+	    
 }
