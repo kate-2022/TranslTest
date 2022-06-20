9d467ca05600904f93f0dd60b21ba2ba7a9bcbab
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-06-20 11:50:15 +0200

    Update AlaTest.java

--- AlaTest.java
+++ AlaTest.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList;
-
 public class AlaTest extends AATest implements  TranslInterface  {
 
             private String peptide;
-	 
-		 	protected TranslInterface nextInChain;
-			protected String singleLetterCode;
-			protected ArrayList<String> codons;
-		//	protected ProtTest aminoAcid = new ProtTest(codons, "");
-			protected String source;
-			private SubTest substrings = new SubTest(source);
-			protected int i;
-			protected int j;
+
+         // all marked instances/variables inherited from AATest superclass !
+            
+		 //	protected TranslInterface nextInChain;
+		 //	protected String singleLetterCode;
+		 //	protected ProtTest aminoAcid;
+         // protected ArrayList<String> codons;
+			private SubTest substrings;
+		//	protected int i;
+		//	protected int j;
 			
 	
 	public AlaTest (String singleLetterCode) {
@@ -19,39 +18,53 @@ public class AlaTest extends AATest implements  TranslInterface  {
 	}
 	
 	
+	@Override
 	public void setNextChain(TranslInterface nextChain) {
 		this.nextInChain = nextChain;	
 	
 	}
 	
  	
-   	public void compareSub(ProtTest aminoAcid) {    
-	
+   	@Override
+	public void compareSub(ProtTest aminoAcid) {    
+   		  codons = substrings.getCodonsSub();
+   	
 		  while( i < codons.size()) {  
 			  if (codons.get(i).equals("GCA")) {
 				singleLetterCode = "A";
 			     i+=1;
-			     System.out.print(singleLetterCode+"_Ala1");		 
+			     System.out.print(singleLetterCode+"_Ala1");	
+			     peptide = peptide + singleLetterCode;
+			     aminoAcid.setPeptide(peptide);
+			     aminoAcid.setCodonsRNA(codons);
 				     }
 			  
 			  else if (codons.get(i).equals("GCC")) {
 				singleLetterCode = "A";
 			     i+=1;
 			     System.out.print(singleLetterCode+"_Ala2");
-		 
+			     peptide = peptide + singleLetterCode;
+			     aminoAcid.setPeptide(peptide);
+			     aminoAcid.setCodonsRNA(codons);
 				     }
 			  
 			  else if (codons.get(i).equals("GCG")) {
 				singleLetterCode = "A";
 			     i+=1;
-			     System.out.print(singleLetterCode+"_Ala3");	 
+			     System.out.print(singleLetterCode+"_Ala3");
+			     peptide = peptide + singleLetterCode;
+			     aminoAcid.setPeptide(peptide);
+			     aminoAcid.setCodonsRNA(codons);
 				     } 
 			  
 			  else if (codons.get(i).equals("GCU")) {
 				singleLetterCode = "A";
 			     i+=1;
 			     System.out.print(singleLetterCode+"_Ala4"); 
-				     }
+			     peptide = peptide + singleLetterCode;
+			     aminoAcid.setPeptide(peptide);
+			     aminoAcid.setCodonsRNA(codons);   
+			     }
 		  
 			  else {                                        
  			  nextInChain.compareSub(aminoAcid);
