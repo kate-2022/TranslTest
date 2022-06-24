f9814c9a0a37cbaf27b9f4eea540513e8cbaf1bd
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-06-24 20:42:51 +0200

    Update MethTest.java

--- MethTest.java
+++ MethTest.java
@@ -9,12 +9,12 @@ public class MethTest extends AATest implements  TranslInterface {
 		    
 		    protected TranslInterface nextInChain;
 		    // protected String singleLetterCode;
-		    protected ProtTest aminoAcid = new ProtTest(codons, ""); 		
-		    //protected ArrayList<String> codons; 
-		    
+		    		
+		    protected ArrayList<String> codons; 
+		    protected ProtTest aminoAcid = new ProtTest(codons, ""); 	
 			// private SubTest substrings = new SubTest("") ;
 			// protected int i;
-			// protected int j;
+		
 
 			
 		// Konstruktor
@@ -31,38 +31,38 @@ public class MethTest extends AATest implements  TranslInterface {
 		
 		@Override
 		public void compareSub(ArrayList<String> codons, String peptide) {  	
-					
-			System.out.println(codons.get(i)+" TestMeth");				
-	    					
+			ArrayList<String> codons1 = new ArrayList<String>();    
+			
+		//	System.out.println(codons.get(i)+" TestMeth");	    	
+				
 			if( i < codons.size()) {  
-				System.out.println("TestMeth1");  
+			//	System.out.println("TestMeth1");  
 			  if (codons.get(i).equals("AUG")) {                                
 				     singleLetterCode = "M";                       
 				     i+=1;	
-				     System.out.println(singleLetterCode + " Meth ");
+				//     System.out.println(codons.get(i)+" testAsc");
+				     
+				//     System.out.println(singleLetterCode + " Meth ");
 				     peptide = peptide + singleLetterCode;
-				     System.out.println("TestMeth2");	
-				     nextInChain.compareSub(codons, peptide);
+				     
+				     codons.remove(0);
+				     codons1 = codons;
+				     nextInChain.compareSub(codons1, peptide);
 				     }		
 	    
 	 		  else {     
-	 			  System.out.println("testPrintMeth");
-	 			  nextInChain.compareSub(codons, peptide);
+	 			//  System.out.println("testPrintMeth");
+	 			  nextInChain.compareSub(codons1, peptide);
 			 
 	 		  }
 		 }
 			else {
-				System.out.println(peptide);
+				  System.out.println("peptide sequence: " + peptide);
 			}
 	 }    
 		
 		
-		
-		@Override
-	    public String toString() {                        // das hier noch überprüfen und bei allen AS Klassen anpassen!!
-			peptide = peptide + singleLetterCode;
-			return peptide;
-	    }
+	
 	     
 	     public String getSingleLetterCode() {
 	 		return singleLetterCode;
