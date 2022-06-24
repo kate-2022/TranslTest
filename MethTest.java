078707c4ee510529ccca0b832ec7715b7844ce06
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-06-24 11:18:17 +0200

    Update MethTest.java

--- MethTest.java
+++ MethTest.java
@@ -32,21 +32,22 @@ public class MethTest extends AATest implements  TranslInterface {
 		public void compareSub(ArrayList<String> codons, String peptide) {  	
 			 //   codons = substrings.getCodonsSub();
 		        
-			System.out.println(codons.get(i)+"TestMeth");
+			System.out.println(codons.get(i)+" TestMeth");
 				
 				codons.get(i);
-		        System.out.println("xy" + codons.contains(i));
-		    	
-				System.out.println("TestMeth1");	
-			while( i < codons.size()) {  
-				System.out.println("TestMeth2");  
+		       // System.out.println("xy" + codons.contains(i));		    	
+				
+			if( i < codons.size()) {  
+				System.out.println("TestMeth1");  
 			  if (codons.get(i).equals("AUG")) {                                
 				     singleLetterCode = "M";                       
 				     i+=1;	
-				     System.out.print(singleLetterCode + " Meth");
+				     System.out.println(singleLetterCode + " Meth ");
 				     peptide = peptide + singleLetterCode;
 				     aminoAcid.setPeptide(peptide);
 				     aminoAcid.setCodonsRNA(codons);
+				     System.out.println("TestMeth2");	
+				     nextInChain.compareSub(codons, peptide);
 				     }		
 	    
 	 		  else {     
@@ -55,6 +56,9 @@ public class MethTest extends AATest implements  TranslInterface {
 			 
 	 		  }
 		 }
+			else {
+				System.out.println("we´re done! :)");
+			}
 	 }    
 		
 		
@@ -92,9 +96,5 @@ public class MethTest extends AATest implements  TranslInterface {
 		public void setAminoAcid(ProtTest aminoAcid) {
 			this.aminoAcid = aminoAcid;
 		}
-
-
-	
-	    
-	    
+    
 }
