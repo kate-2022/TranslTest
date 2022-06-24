c11755b086dba1cf3c37d6ef5e9c4888f9816418
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-06-24 20:37:35 +0200

    Update AlaTest.java

--- AlaTest.java
+++ AlaTest.java
@@ -9,9 +9,9 @@ public class AlaTest extends AATest implements  TranslInterface  {
 		 //	protected TranslInterface nextInChain;
 		 //	protected String singleLetterCode;
 		 //	protected ProtTest aminoAcid;
-       	         //     protected ArrayList<String> codons;
-                 //	protected int i;
-		 //	protected int j;
+      
+         //	protected int i;
+		
 			
 	
 	public AlaTest (String singleLetterCode) {
@@ -28,53 +28,62 @@ public class AlaTest extends AATest implements  TranslInterface  {
  	
    	@Override
 	public void compareSub(ArrayList<String> codons, String peptide) {    
-   		    System.out.println("TestAla");  
-   		    
+	    
 		  if( i < codons.size()) {  
+		//	  System.out.println(codons.get(i)+ " TestAla");  
 			  if (codons.get(i).equals("GCA")) {
 				singleLetterCode = "A";
 			     i+=1;
-			     System.out.print(singleLetterCode+"_Ala1");	
+		
+			//     System.out.print(singleLetterCode+"_Ala1");	
 			     peptide = peptide + singleLetterCode;
-			     System.out.println("TestAla2");	
+			     codons.remove(0);
+			//     System.out.println("TestAla1");	
+			     
 			     nextInChain.compareSub(codons, peptide);
 				     }
 			  
 			  else if (codons.get(i).equals("GCC")) {
 				singleLetterCode = "A";
 			     i+=1;
-			     System.out.print(singleLetterCode+"_Ala2");
+	
+			 //    System.out.println(singleLetterCode+"_Ala2");
 			     peptide = peptide + singleLetterCode;
-			     System.out.println("TestAla3");	
+			     codons.remove(0);
+			//     System.out.println("TestAla2");	
 			     nextInChain.compareSub(codons, peptide);
 				     }
 			  
 			  else if (codons.get(i).equals("GCG")) {
 				singleLetterCode = "A";
 			     i+=1;
-			     System.out.print(singleLetterCode+"_Ala3");
+		
+			//     System.out.println(singleLetterCode+"_Ala3");
 			     peptide = peptide + singleLetterCode;
-			     System.out.println("TestAla4");	
+			     codons.remove(0);
+			//     System.out.println("TestAla3");	
 			     nextInChain.compareSub(codons, peptide);
 				     } 
 			  
 			  else if (codons.get(i).equals("GCU")) {
 				singleLetterCode = "A";
 			     i+=1;
-			     System.out.print(singleLetterCode+"_Ala4"); 
+			
+			//     System.out.println(singleLetterCode+"_Ala4"); 
 			     peptide = peptide + singleLetterCode;
-			     System.out.println("TestAla5");	
+			     codons.remove(0);
+			  //   System.out.println("TestAla4");	
 			     nextInChain.compareSub(codons, peptide);
 			     }
 		  
 			  else {        
-				  System.out.println("testPrintAla");
+				//  System.out.println("testPrintAla");
 				  nextInChain.compareSub(codons, peptide);
 	
 			  }
 		  }
 		  else {
-			  System.out.println(peptide);
+			  System.out.println("peptide sequence: " + peptide);
 			}
 
    	}
@@ -86,14 +95,6 @@ public class AlaTest extends AATest implements  TranslInterface  {
 		return singleLetterCode;
 	}
 
-     
-/*	@Override
-    public String toString() {
-		peptide = peptide + singleLetterCode;
-		return peptide;
-    }
-     */
-
-
+ 
 	
 }
